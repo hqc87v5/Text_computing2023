3073ba20aaffbc009f4a71ec3f477272fc946460
qingcheng.H <hqc87v5@163.com>
2023-11-12 14:37:26 +0800

    dd

--- 作业1-预训练语言模型的词向量/imdb训练.py
+++ 作业1-预训练语言模型的词向量/imdb训练.py
@@ -6,6 +6,10 @@ from tqdm import tqdm
 from typing import IO
 from pathlib import Path
 
+import re
+import six
+import string
+import tarfile
 # 指定保存路径为 `home_path/.mindspore_examples`
 cache_dir = Path.home() / '.mindspore_examples'
 
@@ -29,11 +33,47 @@ def download(file_name: str, url: str):
     cache_exist = os.path.exists(cache_path)
     if not cache_exist:
         with tempfile.NamedTemporaryFile() as temp_file:
+            #tempfile.NamedTemporaryFile() 创建有名字的、在文件系统中可见的临时文件
             http_get(url, temp_file)
-            temp_file.flush()
-            temp_file.seek(0)
+            temp_file.flush() #关闭文件输出流
+            temp_file.seek(0)#seek(0)让指针定位到开头，就可以看到打印输出的内容了。
             with open(cache_path, 'wb') as cache_file:
                 shutil.copyfileobj(temp_file, cache_file)
     return cache_path
 
 imdb_path = download('aclImdb_v1.tar.gz', 'https://mindspore-website.obs.myhuaweicloud.com/notebook/datasets/aclImdb_v1.tar.gz')
+
+class IMDBData():
+    #IMDB数据集加载器，加载IMDB数据集并处理为一个python迭代对象
+    label_map={
+        "pos":1,
+        "neg":0
+    }
+    def __init__(self,path,model="train"):
+        self.mode = model
+        self.path = path
+        self.docs,self.labels = [],[]
+
+        self._load("pos")
+        self._load("neg")
+    def _load(self, label):
+        pattern = re.compile(r"aclImdb/{}/{}/.*\.txt$".format(self.mode,label))
+        #加载数据到内存
+        # tarfile解决zip压缩包的创建、读取、写入
+        .
+        with tarfile.open(self.path) as tarf:
+            tf = tarf.next()
+            while tf is not None:
+                if bool(pattern.match(tf.name)):
+                    self.docs.append(str(tarf.extractfile(tf).read().rstrip(six.b("\n\r")).
+                                         translate(None,six.b(string.punctuation)).lower()).split())
+                    self.labels.append([self.label_map[label]])
+                    tf = tarf.next()
+
+    def __getitem__(self, idx):
+        return self.docs[idx],self.labels[idx]
+    def __len__(self):
+        return len(self.docs)
+
+imdb_train = IMDBData(imdb_path,'train')
+print(imdb_train)
